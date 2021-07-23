5ad25492f939397079cc693a3b12703dfe6b48f0
Svenugopal <47510467+svenugopal333@users.noreply.github.com>
2021-07-23 16:47:40 +1000

    Update InstallPG-ubuntu.sh

--- InstallPG-ubuntu.sh
+++ InstallPG-ubuntu.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+# Install and configure postgres in ubuntu
 sudo apt update -y
 sudo apt install postgresql postgresql-contrib -y
 sudo usermod -aG sudo postgres
 sudo -u postgres createdb testdb
+
+# changing password as postgres for user postgres
+sudo chpasswd <<< "postgres:postgres"  
